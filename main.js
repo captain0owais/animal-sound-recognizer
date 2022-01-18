225644276fc35e11c04b8e404bbda539ed934517
captain0owais <87132814+captain0owais@users.noreply.github.com>
2022-01-18 10:56:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,5 +12,29 @@ function gotResult(error,result){
         console.error(error);
     }else{
         console.log(result);
+        random_r = Math.floor(Math.random()* 255) + 1;
+        random_g = Math.floor(Math.random()* 255) + 1;
+        random_b = Math.floor(Math.random()* 255) + 1;
+
+        document.getElementById("result_label").innerHTML="I can hear - " + result[0].label;
+        document.getElementById("result_accuracy").innerHTML="Accuracy - " + (result[0].confidence * 100).toFixed(2) + "%";
+        document.getElementById("results").style.color = "rgb("+random_r+","+random_g+","+random_b+")";
+
+        img1=document.getElementById("animal_image");
     }
+
+        if(result[0].label=="dog"){
+            img1.src="images (11).jpg";
+
+        }else if(result[0].label=="cat"){
+            img1.src="download.jpg";
+
+        }else if(result[0].label=="cow"){
+            img1.src="download (1).jpg";
+            
+        }else if(result[0].label=="lion"){
+            img1.src="images.png";
+        }else{
+            img1.src="6778-siri-style-loading (1).gif"
+        }
 }
